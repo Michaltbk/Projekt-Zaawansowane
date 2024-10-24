0e677f9bf19cd0597500aca99dd94f2849406b92
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2024-10-24 20:02:11 +0200

    Utworzenie klas: ListaDwuKierunkowa oraz Powiazanie które są podstawą projektu, dodanie podstawowego sprawdzania w postaci funkcji test() ktora ustawia wartości na sztywno oraz funkcji wyswietl() która wyświetla zawartość listy.

--- Projekt1-Zaawansowane/Projekt1-Zaawansowane.cpp
+++ Projekt1-Zaawansowane/Projekt1-Zaawansowane.cpp
@@ -1,20 +1,59 @@
-﻿// Projekt1-Zaawansowane.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+struct Powiazanie {
+	int dane;
+	Powiazanie* nastepny;
+	Powiazanie* poprzedni;
+
+	Powiazanie(int wartosc) : dane(wartosc), nastepny(nullptr), poprzedni(nullptr) {}
+};
+
+class ListaDwuKierunkowa
+{
+private:
+	Powiazanie* przod;
+	Powiazanie* tyl;
+
+public:
+	ListaDwuKierunkowa() : przod(nullptr), tyl(nullptr) {}
+
+	void wyswietl()
+	{
+		Powiazanie* aktualny = przod;
+
+		while (aktualny)
+		{
+			cout << aktualny->dane << " ";
+			aktualny = aktualny->nastepny;
+		}
+		cout<<endl;
+	}
+
+	void test() {
+		Powiazanie* pierwszy = new Powiazanie(2);
+		Powiazanie* drugi = new Powiazanie(6);
+
+		pierwszy->nastepny = drugi;
+		drugi->poprzedni = pierwszy;
+
+		przod = pierwszy;
+		tyl =drugi;
+	}
+
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ListaDwuKierunkowa lista;
+
+	lista.test();
+
+	lista.wyswietl();
+	
+
+	return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
